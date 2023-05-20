b9c4239bdc0c2a4fedc1f3c3c8636a0ff637e16b
Pontus Andersson <102531282+AnderssonP@users.noreply.github.com>
2023-05-20 12:15:01 +0200

    add

---
+++ src/components/productComponents/ProductComponent.jsx
@@ -0,0 +1,18 @@
+export default function Products({id, name, img, price, stock, handleAddToBasket }) {
+
+  return (
+      <div className="productDisplay">
+          <h1>{name}</h1>
+          <img src={img} alt="" />
+          <h2>Pris: {price}</h2>
+          {stock > 0 ? (
+              <div>
+                  <p>Antal kvar: {stock}</p>
+                  <button onClick={handleAddToBasket}>Lägg i kassan</button>
+              </div>
+          ) : (
+              <p>Tyvärr slut i lager</p>
+          )}
+      </div>
+  );
+}
